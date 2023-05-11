cd41ec53dad801430f4fda752aa53a1a43207240
aldijanaa <aldijana.culezovic@stu.ibu.edu.ba>
2023-05-11 12:35:51 +0200

    Deleted engine table from services and routes

--- rest/config.php
+++ rest/config.php
@@ -5,7 +5,7 @@ class Config{
     public static $host = 'localhost';
     public static $database = 'carmarketplace';
     public static $username = 'root';
-    public static $password = 'sifra';
+    public static $password = 'aldijanaaldijana5';
     public static $port = '3306';
 }
 
